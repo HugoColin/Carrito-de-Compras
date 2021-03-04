b7513bbbc43269161b379635ee6a0f41f5d0d192
Hugo Colin <hugo.colin@devitm.com>
2021-03-03 21:00:05 -0600

    Se imprime sin formato los datos de los cursos añadidos al carrito

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const carrito = document.querySelector( '#carrito' );
 const contenedorCarrito = document.querySelector( '#lista-carrito tbody' );
 const vaciarCarritoBtn = document.querySelector( '#vaciar-carrito' );
 const listaCursos = document.querySelector( '#lista-cursos' );
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners(){
@@ -26,7 +27,8 @@ function agregarCurso( e ){
 
 // lee el contenido del HTML al que le dimos click y extrae la información del curso
 function leerDatosCurso( curso ) {
-    console.log( curso );
+
+    // console.log( curso );
 
     // crear un objeto con contenido del curso del curso actual
     const infoCurso = {
@@ -34,8 +36,46 @@ function leerDatosCurso( curso ) {
         titulo: curso.querySelector( 'h4' ).textContent,
         precio: curso.querySelector( '.precio span' ).textContent,
         id: curso.querySelector( 'a' ).getAttribute( 'data-id' ),
-        cantidad: 1
+        cantidad: 1,
     }
 
-    console.log( infoCurso );
+    // Agrega elementos al arreglo de carrito
+    // articulosCarrito = [ ...articulosCarrito, infoCurso ];
+    articulosCarrito.push( infoCurso );
+
+    console.log( articulosCarrito );
+
+    carritoHTML();
+}
+
+// Muestra el carrito de compras en el HTML
+function carritoHTML() {
+
+    // Limpiar el HTML
+    limpiarHTML();
+
+    // Recorre el carrito y genera el HTML
+    articulosCarrito.forEach( curso => {
+        const row = document.createElement( 'tr' );
+        row.innerHTML = `
+            <td>
+                ${ curso.titulo }
+            </td>
+        `;
+
+        // Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild( row );
+
+    } );
+}
+
+// Elimina los cursos del tbody
+function limpiarHTML() {
+
+    // Forma lenta
+    // contenedorCarrito.innerHTML = '';
+
+    while ( contenedorCarrito.firstChild ){
+        contenedorCarrito.removeChild( contenedorCarrito.firstChild );
+    }
 }
